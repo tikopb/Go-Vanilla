c7a096ee66451964318e973c6f052761d060f193
“TIKO <tiko.pb@gmail.com>
2023-11-01 13:35:16 +0700

    Delete Seed

--- internal/database/seed.go
+++ internal/database/seed.go
@@ -9,12 +9,5 @@ import (
 func seedDB(db *gorm.DB) {
 	db.AutoMigrate(
 		&model.User{},
-		&model.Partner{},
-		&model.Invoice{},
-		&model.InvoiceLine{},
-		&model.Product{},
-		&model.Payment{},
-		&model.PaymentLine{},
-		&model.DocumentNoTemp{},
 	)
 }
